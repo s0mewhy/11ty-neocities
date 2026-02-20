b484d00837d84527a2c81e328756aa792425a889
s0mewhy <s0mewhy.art@gmail.com>
2026-02-20 09:16:53 -0600

    add art tags, work on blog layout, style changes

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,18 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy({ './src/robots.txt': '/robots.txt' });
-    eleventyConfig.addPassthroughCopy("./src/css");
+    eleventyConfig.addPassthroughCopy("./src/_includes/css");
     eleventyConfig.addPassthroughCopy("./src/img");
-    eleventyConfig.addPassthroughCopy("./src/fonts");
-    eleventyConfig.addPassthroughCopy("./src/js");
 
+    eleventyConfig.addFilter("date", require("./src/_filters/date.js"));
+
+/*     eleventyConfig.addGlobalData("arttags", () => {
+        let tags = new Array();
+        for(let i = 0; i < allart.length; i++) {
+            tags = tags.concat(allart[i].tags);
+        }
+		return tags.sort();
+	});
+    */
     return {
         passthroughFileCopy: true,
         dir: {
