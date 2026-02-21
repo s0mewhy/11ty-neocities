897126ce77436316b10527b24884ba53c89902d0
s0mewhy <s0mewhy.art@gmail.com>
2026-02-20 23:09:48 -0600

    add search by tags/year to gallery, add more art tags, add rimraf to clean up /public dir

--- .eleventy.js
+++ .eleventy.js
@@ -1,21 +1,32 @@
+// clean /public dir
+const rimraf = require("rimraf");
+
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy({ './src/robots.txt': '/robots.txt' });
+    //clean up output dir
+    rimraf.windows.sync("public/");
+
+    eleventyConfig.addPassthroughCopy({"./src/robots.txt": "/robots.txt"});
     eleventyConfig.addPassthroughCopy("./src/_includes/css");
     eleventyConfig.addPassthroughCopy("./src/img");
 
     eleventyConfig.addFilter("date", require("./src/_filters/date.js"));
 
-/*     eleventyConfig.addGlobalData("arttags", () => {
+    eleventyConfig.addCollection("arttags", function (collection) {
         let tags = new Array();
-        for(let i = 0; i < allart.length; i++) {
-            tags = tags.concat(allart[i].tags);
-        }
-		return tags.sort();
-	});
-    */
+        for (let a of collection.getAll()[0].data.allart) {
+            for (let t of a.tags){
+                if (!tags.includes(t)){
+                    tags.push(t);
+                }
+            }
+        } 
+        return tags.sort();
+    });
+
     return {
         passthroughFileCopy: true,
         dir: {
+            data: "_data",
             input: "src",
             output: "public",
             includes: "_includes",
